fde039602c5b34b4d85f375f547eb685c6578be8
Vivek Nigam <viveknigam.nigam3@gmail.com>
2019-07-23 10:31:42 +0530

    Lab-2

--- Newton Raphson/main.c
+++ Newton Raphson/main.c
@@ -7,6 +7,7 @@ Lab - Numerical Methods Lab - 2
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define TOL 10e-4
 
 double func (double x)
 {
@@ -20,16 +21,20 @@ double derfunc (double x)
 
 int main()
 {
-    double x,x1,eps;
-    x=3.0;
+    double x,x1;
+    x1=10.0;
     int itr=1;
+    printf("\nInitial Guess: %lf\n", x1);
+    
+    printf("\n x \t\t x1 \t\t f'(x) \t\t |x-x1|\n", x,x1);
     
     do
     {
         x=x1;
+        printf("\n %lf \t %lf \t %lf \t %lf\n", x,x1, derfunc(x) ,(x-x1));
+        
         if (fabs(derfunc(x))>=10e-8 && derfunc(x) != NAN)
         {
-            printf("Inside if\n");
             x1 = x - func(x)/derfunc(x);
             itr++;
         }
@@ -38,7 +43,7 @@ int main()
             printf("Method Fails");
             return 0;
         }
-    }while (fabs(x-x1) >= eps);
+    }while (fabs(x-x1) >= TOL);
     
     printf("\nRoot = %f\n", x1);
     printf("Number of Iterations = %d", itr);
