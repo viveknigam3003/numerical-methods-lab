f73bce7f5f85f44c2d23b890a84be01c8b970a55
Vivek Nigam <viveknigam.nigam3@gmail.com>
2019-10-22 10:44:08 +0530

    Lab-9

--- GaussSeidel.c
+++ GaussSeidel.c
@@ -1,5 +1,4 @@
-
- #include<stdio.h>
+#include<stdio.h>
 #include<math.h>
 #define ESP 0.0001
 #define X1(x2,x3,x4) ((-2 - x2 + x3 - x4)/4)
@@ -13,9 +12,7 @@ void main()
   double x1=0,x2=0,x3=0,x4=0,y1,y2,y3,y4,m,n,p,o;
   int i=0;
 
-  printf("\n__________________________________________\n");
   printf("\n   x1\t\t   x2\t\t    x3\t\t   x4\n");
-  printf("\n__________________________________________\n");
   printf("\n%f\t%f\t%f\t%f",x1,x2,x3,x4);
   do
   {
@@ -30,7 +27,6 @@ void main()
    printf("\n%f\t%f\t%f\t%f",x1,x2,x3,x4);
    if(fabs(m-x1)<ESP && fabs(n-x2)<ESP && fabs(o-x3)<ESP && fabs(p-x4)<ESP )
    {
-     printf("\n__________________________________________\n");
      printf("\n\nx1 = %.3lf",x1);
      printf("\n\nx2 = %.3lf",x2);
      printf("\n\nx3 = %.3lf",x3);
